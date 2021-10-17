7f3adf01624ff97aec1e1855e67ac61fe7b86ab8
Jarvis-Geun <jarvis.geun@gmail.com>
2021-10-17 17:17:11 +0900

    2021.10.17(SUN)

---
+++ WEB2-JavaScript_11/practice.js
@@ -0,0 +1,28 @@
+function setColor(color)    {
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while(i < alist.length) {
+        alist[i].style.color = color;
+        i += 1;
+    }
+}
+
+function NightDayHandler(parameter) {
+var target = document.querySelector('body');
+
+if(parameter.value == 'night')   {
+    target.style.backgroundColor = 'black';
+    target.style.color = 'white';
+    parameter.value = 'day';
+    
+    setColor('powderblue');
+}
+
+else    {
+    target.style.backgroundColor = 'white';
+    target.style.color = 'black';
+    parameter.value = 'night';
+
+    setColor('blue');
+}
+}
